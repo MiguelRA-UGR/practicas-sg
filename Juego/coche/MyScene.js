3a527abc80fc9391d5b6a8ac5bf89f4927c97fa5
MiguelRA-UGR <e.miguelra@go.ugr.es>
2024-04-05 17:26:16 +0200

    Commit realizado el 2024-04-05 17:26:14

--- Juego/coche/MyScene.js
+++ Juego/coche/MyScene.js
@@ -21,7 +21,7 @@ class MyScene extends THREE.Scene {
     this.add (this.axis);
     this.createLights ();
     this.createCamera ();
-    //this.createGround ();
+    this.createCircuito();
 
     //Modelos
   ////////////////////////////////////////////////////////////////////
@@ -32,11 +32,10 @@ class MyScene extends THREE.Scene {
     //this.add(this.escorvispa);
     //*/
    //////////////////////////////////////////////////////////////////
-    /*
+    
     this.coche = new Coche(this.gui, "Coche");
-    //this.coche.rotateY(THREE.MathUtils.degToRad(180));
     this.add(this.coche);
-    */
+    
   ////////////////////////////////////////////////////////////////////
     /*
     this.minigun = new Minigun(this.gui, "Minigun")
@@ -45,26 +44,41 @@ class MyScene extends THREE.Scene {
   ////////////////////////////////////////////////////////////////////
     /*
     this.paredGeom = new THREE.BoxGeometry(5,2,0.2);
-    const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+    const material = new THREE.MeshStandardMaterial({ color: 0x0000ff });
     this.pared = new THREE.Mesh(this.paredGeom,material);
-    this.pared.position.set(0,1,5);
+    this.pared.position.set(0,1,4);
     this.add(this.pared);
     */
   ////////////////////////////////////////////////////////////////////
-    //this.orbe1 = new Orbe(TipoOrbe.TIEMPO_RALENTIZADO);
-    //this.orbe1.scale.set(0.7,0.7,0.7);
+    /*
+    this.orbe1 = new Orbe(TipoOrbe.DAÑO_AUMENTADO);
+    this.orbe1.scale.set(3,3,3);
     //this.orbe1.position.set(3,0,0);
-    //this.add(this.orbe1);
+    this.add(this.orbe1);
+    */
   ////////////////////////////////////////////////////////////////////
-    this.bala = new Bala();
-    this.bala.position.set(2,0,0);
-    //this.add(this.bala);
 
-    this.babosa = new Babosa();
-    this.add(this.babosa);
+    //this.babosa = new Babosa();
+    //this.add(this.babosa);
 
   }
   
+  createCircuito(){
+    var textureLoader = new THREE.TextureLoader();
+    var texture = textureLoader.load('../imgs/colmena.jpg');
+		this.materialColmena = new THREE.MeshBasicMaterial({ map: texture});
+
+    var material = new THREE.MeshStandardMaterial( {
+      normalMap: texture, // Asigna la textura normal
+      // Puedes ajustar la intensidad del relieve si lo deseas
+      normalScale: new THREE.Vector2( 1, 1 ) // Ajusta según tu preferencia
+  } );
+
+    this.circuitoGeom = new THREE.TorusKnotGeometry(20,4,50,20,2,3);
+    this.circuitoKnot = new THREE.Mesh(this.circuitoGeom,material);
+    this.add(this.circuitoKnot);
+  }
+
   initStats() {
     var stats = new Stats();
     stats.setMode(0);
@@ -75,8 +89,37 @@ class MyScene extends THREE.Scene {
     this.stats = stats;
   }
   
+  getPathFromTorusKnot (torusKnot) {
+    // La codificación de este método está basado
+    //   en el código fuente de la página oficial de Three.js
+    // https://github.com/mrdoob/three.js/blob/master/src/geometries/TorusKnotGeometry.js
+    const p = torusKnot.parameters.p;
+    const q = torusKnot.parameters.q;
+    const radius = torusKnot.parameters.radius;
+    const resolution = torusKnot.parameters.tubularSegments;
+    var u, cu, su, quOverP, cs;
+    var x,y,z;
+    // En  points  se almacenan los puntos que extraemos del torusKnot
+    const points = [];
+    for ( let i = 0; i < resolution; ++ i ) {
+			u = i / resolution * p * Math.PI * 2;
+      cu = Math.cos( u );
+			su = Math.sin( u );
+			quOverP = q / p * u;
+			cs = Math.cos( quOverP );
+
+			x = radius * ( 2 + cs ) * 0.5 * cu;
+			y = radius * ( 2 + cs ) * su * 0.5;
+			z = radius * Math.sin( quOverP ) * 0.5;
+
+      points.push (new THREE.Vector3 (x,y,z));
+    }
+    // Una vez tenemos el array de puntos 3D construimos y devolvemos el CatmullRomCurve3
+    return new THREE.CatmullRomCurve3 (points, true);
+  }
+
   createCamera () {
-    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 50);
+    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
     this.camera.position.set (4, 2, 4);
     var look = new THREE.Vector3 (0,0,0);
     this.camera.lookAt(look);
@@ -86,15 +129,6 @@ class MyScene extends THREE.Scene {
     this.cameraControl.zoomSpeed = -2;
     this.cameraControl.panSpeed = 0.5;
     this.cameraControl.target = look;
-  }
-  
-  createGround () {
-    var geometryGround = new THREE.BoxGeometry (10,0.2,10);
-    var texture = new THREE.TextureLoader().load('../imgs/wood.jpg');
-    var materialGround = new THREE.MeshStandardMaterial ({map: texture});
-    var ground = new THREE.Mesh (geometryGround, materialGround);
-    ground.position.y = -0.1;
-    this.add (ground);
   }
   
   createGUI () {
@@ -140,7 +174,7 @@ class MyScene extends THREE.Scene {
   
   createRenderer (myCanvas) {
     var renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(new THREE.Color(0xFFFFFF), 1.0);
+    renderer.setClearColor(new THREE.Color(0x000000), 1.0);
     renderer.setSize(window.innerWidth, window.innerHeight);
     $(myCanvas).append(renderer.domElement);
     return renderer;  
@@ -168,7 +202,7 @@ class MyScene extends THREE.Scene {
     //this.minigun.update();
 
    //this.orbe1.update();
-   this.babosa.update();
+   //this.babosa.update();
 
     this.renderer.render (this, this.getCamera());
     requestAnimationFrame(() => this.update())
